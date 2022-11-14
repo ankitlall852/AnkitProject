64fa851887d9272939cf200c7f19411e38073b61
Ankit <ankitlall852@gmail.com>
2022-11-13 21:19:07 -0500

    commit test

---
+++ Test.py
@@ -0,0 +1,28 @@
+#Test.
+#1) Write a program to find the sum of 3 no entered by user
+# a=int(input("Enter first no"))
+# b=int(input("Enter second  no"))
+# c=int(input("Enter third  no"))
+# d=a+b+c
+# print(d)
+
+# 2) Write a program to find the pattern of start
+for n in range(1,6):
+    for x in range(n):
+        print("*",end="")
+        #what does end do again, does it change vertical to horizontal
+    print()
+
+#3) Write a program to find the sum of all the elements of an array
+#without using numpy
+# a=[1,2,3,4]
+# b=a[0]+a[1]+a[2]+a[3]
+# print(b)
+
+#using numpy write a program to convert one array into
+#2 dimension and the print sum fo that 2 d array
+# a=np.array([1,2,3,4,5,3])
+# b=a.reshape(2,3)
+# print(b)
+# print("the sum of the 2d array is ", sum.(b))
+#what is the code for sum.
